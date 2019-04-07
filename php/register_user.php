2ff649a8947c5ba782c5dd29f9bcbecbfb1a566b
McPitris <petras.michal98@gmail.com>
2019-04-07 22:00:59 +0200

    předělání hashování hesla -> přehlednost

--- php/register_user.php
+++ php/register_user.php
@@ -8,9 +8,9 @@
 
          include("connect_dbs.php");
          if (isset($_POST["submit_register"])) {
-             $hash_pw = "{$_POST['password']}"; //zahashování
+             $hash_pw = md5("{$_POST['password']}"); //zahashování
              $sql_add = "INSERT INTO Uzivatele (krestni, prijmeni, nick, heslo, kredity, opravneni) VALUES
-    ('{$_POST['name']}', '{$_POST['surname']}', '{$_POST['nick']}',md5($hash_pw), 0, 2)"; //přidání uživatele do databáze s 0 kredity a opravněním 2 (uživatel)
+    ('{$_POST['name']}', '{$_POST['surname']}', '{$_POST['nick']}',$hash_pw, 0, 2)"; //přidání uživatele do databáze s 0 kredity a opravněním 2 (uživatel)
 
              if ($conn->query($sql_add) == true) {
                  echo "<script>alert('Uživatel byl zaregistován!');</script>";
