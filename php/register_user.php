c149c4c90773c4c90fce8dd83d5fba01dcdd02ce
McPitris <petras.michal98@gmail.com>
2019-04-07 21:54:13 +0200

    hashování hesla a popis

--- php/register_user.php
+++ php/register_user.php
@@ -8,8 +8,9 @@
 
          include("connect_dbs.php");
          if (isset($_POST["submit_register"])) {
+             $hash_pw = "{$_POST['password']}"; //zahashování
              $sql_add = "INSERT INTO Uzivatele (krestni, prijmeni, nick, heslo, kredity, opravneni) VALUES
-    ('{$_POST['name']}', '{$_POST['surname']}', '{$_POST['nick']}','{$_POST['password']}', 0, 2)";
+    ('{$_POST['name']}', '{$_POST['surname']}', '{$_POST['nick']}',md5($hash_pw), 0, 2)"; //přidání uživatele do databáze s 0 kredity a opravněním 2 (uživatel)
 
              if ($conn->query($sql_add) == true) {
                  echo "<script>alert('Uživatel byl zaregistován!');</script>";
